6d77766f64d57f4bad348e8809f73a62863537d5
cetius78 <cetius@yahoo.com>
2024-12-29 13:20:21 -0600

    player movement

--- main.py
+++ main.py
@@ -4,17 +4,26 @@
 import pygame
 #module imports
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
+    gameClock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                #pygame.quit()
+                #quit()
                 return
         screen.fill("black")
-        #final step
+        player.update(dt)
+        player.draw(screen)
+        #final steps
         pygame.display.flip()
+        dt = gameClock.tick(60)/1000
 
 
     print("Starting asteroids!")
